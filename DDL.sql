1be197e07cf1f2f062fcd8abee03ed21225253f9
IvanKushc <kush73reg@yandex.ru>
2024-09-06 16:34:12 +0300

    Update DDL.sql
    Adding a new table brands_origins

--- DDL.sql
+++ DDL.sql
@@ -1,14 +1,20 @@
 CREATE SCHEMA car_shop;
 
+CREATE TABLE car_shop.brand_origins (
+	id SERIAL PRIMARY KEY,
+	"name" VARCHAR(20) UNIQUE); -- Название страны производителя
+
 CREATE TABLE car_shop.brands ( -- Таблица с моделями авто
 	id SERIAL PRIMARY KEY, -- Идентификатор
 	name VARCHAR(30) UNIQUE NOT NULL, -- Производителя длинне 30 символов я нагуглить не смог
-	brand_origin VARCHAR(20)); -- Страны длиннее 20 символов тоже нет
-	
+	brand_origin_id int REFERENCES car_shop.brand_origins);
+
 CREATE TABLE car_shop.models (
 	id SERIAL PRIMARY KEY,
-	name VARCHAR(50) UNIQUE NOT NULL); -- Судя по гуглу, самое длинное название "BMW 7 Series 740Li xDrive with M Sport Package" - 43 символа
-	
+	name VARCHAR(50) UNIQUE NOT NULL, -- Судя по гуглу, самое длинное название "BMW 7 Series 740Li xDrive with M Sport Package" - 43 символа
+	gasoline_consumption NUMERIC,
+	brand_id int NOT NULL REFERENCES car_shop.brands);
+
 CREATE TABLE car_shop.colors (
 	id SERIAL PRIMARY KEY,
 	name VARCHAR(40) UNIQUE NOT NULL); -- Самый длинный цвет 40 символов "Luminous Framework for the Color Red"
@@ -17,14 +23,13 @@ CREATE TABLE car_shop.cars (
 	id SERIAL PRIMARY KEY,
 	brand_id int REFERENCES car_shop.brands,
 	model_id int REFERENCES car_shop.models,
-	color_id int REFERENCES car_shop.colors,
-	gasoline_consumption numeric); -- Дробное значение
+	color_id int REFERENCES car_shop.colors);
 	
 CREATE TABLE car_shop.customers (
 	id SERIAL PRIMARY KEY,
 	first_name TEXT NOT NULL, -- Подразумевается ввод имени
 	last_name TEXT NOT NULL, -- Подразумевает ввод фамилии
-	phone TEXT UNIQUE); -- Подразумевает ввод телефона
+	phone TEXT); -- Подразумевает ввод телефона
 	
 CREATE TABLE car_shop.sales (
 	id SERIAL PRIMARY KEY,
