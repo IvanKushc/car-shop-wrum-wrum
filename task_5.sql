848643734450804615dbae87855f94cbcea9ad3a
IvanKushc <kush73reg@yandex.ru>
2024-09-06 16:49:25 +0300

    Update task_5.sql

--- task_5.sql
+++ task_5.sql
@@ -1,10 +1,13 @@
-SELECT b.brand_origin, -- Задание 5 из 6
-	MIN(s.price) price_max, 
-	MAX(s.price) price_min
-FROM car_shop.sales s
+SELECT bo."name",
+	MIN(s.price + (s.discount::NUMERIC / 100 * s.price)::NUMERIC) price_min,
+	MAX(s.price + (s.discount::NUMERIC / 100 * s.price)::NUMERIC) price_max
+FROM car_shop.sales s 
 JOIN car_shop.cars c 
-	ON s.car_id = c.id 
+	ON s.car_id = c.id
 JOIN car_shop.brands b 
-	ON c.brand_id = b.id 
-WHERE s.discount != '0' AND b.brand_origin IS NOT NULL 
-GROUP BY b.brand_origin;
+	ON c.brand_id = b.id
+JOIN car_shop.brand_origins bo 
+	ON b.brand_origin_id = bo.id 
+WHERE bo."name" IS NOT NULL
+GROUP BY bo."name"
+ORDER BY bo."name" ;
