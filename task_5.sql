8eafb92dca08f09196f53c56059ec70998d2577a
IvanKushc <kush73reg@yandex.ru>
2024-09-07 23:26:02 +0300

    Update task_5.sql
    udate formula

--- task_5.sql
+++ task_5.sql
@@ -1,13 +1,15 @@
 SELECT bo."name",
-	MIN(s.price + (s.discount::NUMERIC / 100 * s.price)::NUMERIC) price_min,
-	MAX(s.price + (s.discount::NUMERIC / 100 * s.price)::NUMERIC) price_max
+	MIN(s.price / (1 - (s.discount / 100))) price_min,
+	MAX(s.price / (1 - (s.discount / 100))) price_max
 FROM car_shop.sales s 
 JOIN car_shop.cars c 
 	ON s.car_id = c.id
 JOIN car_shop.brands b 
-	ON c.brand_id = b.id
+JOIN car_shop.models m 
+	ON m.brand_id = b.id 
+	ON c.model_id = m.id 
 JOIN car_shop.brand_origins bo 
-	ON b.brand_origin_id = bo.id 
+	ON b.brand_origin_id = bo.id
 WHERE bo."name" IS NOT NULL
 GROUP BY bo."name"
-ORDER BY bo."name" ;
+ORDER BY bo."name";
